a2d28451bdb2ae0a27526304a8527d6531630b37
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-13 19:50:55 +0530

    Added : mapped project to ProjectGrid

--- src/components/Content/CheckList/ProjectsGrid/EachProject/EachProject.jsx
+++ src/components/Content/CheckList/ProjectsGrid/EachProject/EachProject.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import useStyles from "./styles";
 import { Paper, Divider , Typography} from "@material-ui/core";
 
-const EachProject = () => {
+const EachProject = ({eachProject}) => {
   const classes = useStyles();
 
   return (
     <Paper elevation={6} className={classes.root}>
-      <Typography color="primary" variant="h5" align="center">heading</Typography>
+      <Typography color="primary" variant="h5" align="center">{eachProject?.name}</Typography>
     <Divider className={classes.divider} />
     hello
     </Paper>
--- src/components/Content/CheckList/ProjectsGrid/ProjectsGrid.jsx
+++ src/components/Content/CheckList/ProjectsGrid/ProjectsGrid.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import {useContext} from "react";
 import EachProject from "./EachProject/EachProject";
 import {Grid} from "@material-ui/core";
+import { ProjectsContext } from '../../../../contexts/ProjectsContext';
 
 const ProjectsGrid = () => {
+  const {project} = useContext(ProjectsContext);
+  console.log("projectsfjdfkjdlkfjasdk", project)
   return (
-    <Grid container>
-      <Grid item xs={12} sm={6} md={6} lg={4}>
-        <EachProject />
-      </Grid>
+    <Grid container spacing={6}>
+      {project.map(eachProject=> 
+      <Grid item xs={6} sm={6} md={6} lg={4}>
+        <EachProject eachProject={eachProject}/>
+        {/* <EachProject /> */}
+
+      </Grid>)
+}
     </Grid>
   );
 };
