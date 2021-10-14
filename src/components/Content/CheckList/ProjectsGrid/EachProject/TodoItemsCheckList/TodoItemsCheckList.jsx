2bd8e7ffc4a9dd787bb2c3cac0d1ed1f342fbe9a
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-14 11:07:31 +0530

    Added : checkbox

--- src/components/Content/CheckList/ProjectsGrid/EachProject/TodoItemsCheckList/TodoItemsCheckList.jsx
+++ src/components/Content/CheckList/ProjectsGrid/EachProject/TodoItemsCheckList/TodoItemsCheckList.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import useStyles from "./styles";
 import {
   ListItem,
@@ -9,38 +9,51 @@ import {
   ListItemText,
   IconButton,
 } from "@material-ui/core";
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 import FolderIcon from "@material-ui/icons/Folder";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { ProjectsContext } from '../../../../../../contexts/ProjectsContext';
+import { ProjectsContext } from "../../../../../../contexts/ProjectsContext";
 
-const TodoItemsCheckList = ({eachProject}) => {
+const TodoItemsCheckList = ({ eachProject }) => {
   const classes = useStyles();
   //  )
   console.log(eachProject);
 
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <List dense={true}>
-         {eachProject.todoItems.map(eachItem =>
-
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
+      {eachProject.todoItems.map((eachItem) => (
+        <ListItem key={eachItem.name}>
+          <ListItemAvatar>
+            {/* <Avatar>
             <FolderIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={eachItem.name}
-          // secondary={secondary ? 'Secondary text' : null}
-        />
-        <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>)}
+          </Avatar> */}
+
+            <Checkbox
+              defaultChecked={checked}
+              color="primary"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+          </ListItemAvatar>
+          <ListItemText
+            primary={eachItem.name}
+            // secondary={secondary ? 'Secondary text' : null}
+          />
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
     </List>
-  
   );
 };
 
