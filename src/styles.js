2322d1b86a4202c1eff31d9791563a44f8361e02
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-15 20:44:17 +0530

    style

--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,18 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme=>({
     outerBox:{
         display : "flex",
         justifyContent : "center",
-        alignItems : "center"
+        alignItems : "center",
+        [theme.breakpoints.down('sm')]: {
+            // backgroundColor: theme.palette.secondary.main,
+          },
+          [theme.breakpoints.up('sm')]: {
+            overflowX : "hidden"
+        },
     }
-});
+}));
 
 export default useStyles;
 
