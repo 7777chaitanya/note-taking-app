77f4a7ab1c68e288091342c89e0c1dd3240be7f8
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-16 19:36:31 +0530

    styles

--- src/components/Content/ScratchPad/ScratchPadBody/styles.js
+++ src/components/Content/ScratchPad/ScratchPadBody/styles.js
@@ -1,58 +1,54 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    margin: {
-      margin: theme.spacing(4),
-     
-    },
-    withoutLabel: {
-      marginTop: theme.spacing(3),
-    },
-    textField: {
-      width: '25ch',
-    },
-    outerBox : {
-        maxWidth : '75vw',
-        maxHeight : "80vh",
-        // overflowY : "auto"
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  margin: {
+    margin: theme.spacing(4),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: "25ch",
+  },
+  outerBox: {
+    maxWidth: "75vw",
+    maxHeight: "80vh",
+    // overflowY : "auto"
+  },
+  textArea: {
+    // overflowY : "scroll",
+    // maxHeight : "60vh"
+  },
 
-    },
-    textArea:{
-        // overflowY : "scroll",
-        // maxHeight : "60vh"
-    },
-    
-    input:{
-        height : "3rem",
-        width : "100%",
-        fontSize : ""
-    },
-    noteTitleInput:{
-        height : "3rem",
-         fontSize : "2.5rem",
-        //  color  :"green",
-         width : "100%",
-        //  paddingTop : "2rem",
-         marginTop : "rem"
-     },
-     textAreaBox:{
-        //  marginTop : "8rem"
-     },
-     outerPaper:{
-        maxWidth : "60vw",
-        minHeight : "80vh",
-        margin : "auto",
-        // backgroundColor : 'lightgray',
-        paddingTop : "2rem",
-        paddingLeft : "1rem",
-        paddingRight : "1rem"
-    },
-  }));
+  input: {
+    height: "3rem",
+    width: "100%",
+    fontSize: "",
+  },
+  noteTitleInput: {
+    height: "3rem",
+    fontSize: "1.5rem",
+    //  color  :"green",
+    width: "100%",
+    //  paddingTop : "2rem",
+    marginTop: "rem",
+  },
+  textAreaBox: {
+    //  marginTop : "8rem"
+  },
+  outerPaper: {
+    maxWidth: "60vw",
+    minHeight: "80vh",
+    margin: "auto",
+    // backgroundColor : 'lightgray',
+    paddingTop: "2rem",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+  },
+}));
 
 export default useStyles;
-
-
