8841ca9bae9c6e43b773b46bcf0756c136b453f2
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-16 14:10:00 +0530

    Added : scroll to project checklist

--- src/components/Content/CheckList/ProjectsGrid/ProjectsGrid.jsx
+++ src/components/Content/CheckList/ProjectsGrid/ProjectsGrid.jsx
@@ -2,12 +2,14 @@ import {useContext} from "react";
 import EachProject from "./EachProject/EachProject";
 import {Grid} from "@material-ui/core";
 import { ProjectsContext } from '../../../../contexts/ProjectsContext';
+import useStyles from "./styles";
 
 const ProjectsGrid = () => {
+  const classes = useStyles();
   const {project} = useContext(ProjectsContext);
   console.log("projectsfjdfkjdlkfjasdk", project)
   return (
-    <Grid container spacing={6}>
+    <Grid container spacing={6} className={classes.cardItemsGrid}>
       {project.map(eachProject=> 
       <Grid item xs={6} sm={6} md={6} lg={4} key={eachProject.id}>
         <EachProject eachProject={eachProject}/>
---
+++ src/components/Content/CheckList/ProjectsGrid/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    cardItemsGrid:{
+        maxHeight : "81vh",
+        overflowY : "scroll",
+        marginTop : "1rem"
+    }
+})
+
+export default useStyles;
