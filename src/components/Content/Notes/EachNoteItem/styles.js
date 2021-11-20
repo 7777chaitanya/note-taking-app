dd47837cc4b3f1698aa977cb86f0246cbcd40f6f
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-20 20:30:30 +0530

    refactored

--- src/components/Content/Notes/EachNoteItem/styles.js
+++ src/components/Content/Notes/EachNoteItem/styles.js
@@ -1,25 +1,23 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-root : {
-    width : "100%",
-    padding : "1rem",
+  root: {
+    width: "100%",
+    padding: "1rem",
     "&:hover": {
-        cursor : "pointer",
-        backgroundColor: 'rgb(7, 177, 77,0.50)',
-        // backgroundColor : "rgb(220,220,220)"
-      },
-      maxWidth : "13rem",
-      overFlowX : "scroll"
-},
-divider:{
-  margin : "0.5rem"
-},
-eachNoteBox:{
-  overflowWrap : "break-word"
-}
+      cursor: "pointer",
+      backgroundColor: "rgb(7, 177, 77,0.50)",
+      // backgroundColor : "rgb(220,220,220)"
+    },
+    maxWidth: "13rem",
+    overFlowX: "scroll",
+  },
+  divider: {
+    margin: "0.5rem",
+  },
+  eachNoteBox: {
+    overflowWrap: "break-word",
+  },
 });
 
 export default useStyles;
-
-
