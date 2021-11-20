5cb34408ca3f0963074fccd52868fc75f76e2f1c
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-20 18:13:38 +0530

    Modified : scroll bar color in dark mode

--- src/App.js
+++ src/App.js
@@ -10,20 +10,56 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 const App = () => {
   const classes = useStyles();
   const { notes } = useContext(NotesContext);
-  console.log("notes => ", notes);
-
   const [darkMode, setDarkMode] = useState(false)
-  console.log('darkMode in app => ', darkMode)
 
-  const theme=createTheme({
+  const theme = createTheme({
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          body: {
+            scrollbarColor: !darkMode ? "#3f51b5 #ffffff" : "#3f51b5 #959595",
+            "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+              backgroundColor: !darkMode ? "#ffffff" : "#424242",
+              width : "0.3rem"
+            },
+            "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+              borderRadius: 8,
+              backgroundColor: "#3f51b5",
+              minHeight: 24,
+              // border: "1px solid #ffffff",
+              
+            },
+            "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus":
+              {
+                backgroundColor: "#959595",
+              },
+            "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active":
+              {
+                backgroundColor: "#959595",
+              },
+            "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
+              {
+                backgroundColor: "#959595",
+              },
+            "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+              backgroundColor: "#ffffff",
+            },
+          },
+        },
+       
+      },
+     
+    },
     palette : {
       type : darkMode ? "dark" : "light"
     }
-  })
+    
+  });
   return (
     <Router>
-      <CssBaseline />
       <ThemeProvider theme={theme}>
+      <CssBaseline />
+      
 
       <Box className={classes.outerBox}>
         
--- src/index.js
+++ src/index.js
@@ -7,44 +7,9 @@ import { ProjectsProvider } from "./contexts/ProjectsContext";
 import { createTheme, ThemeProvider } from "@material-ui/core";
 import { ScratchPadProvider } from "./contexts/ScratchPadContext";
 
-const theme = createTheme({
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        body: {
-          scrollbarColor: "#3f51b5 #ffffff",
-          "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
-            backgroundColor: "#ffffff",
-          },
-          "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
-            borderRadius: 8,
-            backgroundColor: "#3f51b5",
-            minHeight: 24,
-            border: "3px solid #ffffff",
-          },
-          "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus":
-            {
-              backgroundColor: "#959595",
-            },
-          "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active":
-            {
-              backgroundColor: "#959595",
-            },
-          "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
-            {
-              backgroundColor: "#959595",
-            },
-          "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
-            backgroundColor: "#ffffff",
-          },
-        },
-      },
-    },
-  },
-});
+
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
     <ScratchPadProvider>
     <NotesProvider>
       <TrashNotesProvider>
@@ -53,7 +18,6 @@ ReactDOM.render(
         </ProjectsProvider>
       </TrashNotesProvider>
     </NotesProvider>
-    </ScratchPadProvider>
-  </ThemeProvider>,
+    </ScratchPadProvider>,
   document.getElementById("root")
 );
