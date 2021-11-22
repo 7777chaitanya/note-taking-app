f78876ed48f405118380f6febba742e790ad4ed5
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-22 10:25:55 +0530

    Added : New note snackbar

--- src/components/LeftMenu/NewNoteButton/NewNoteButton.jsx
+++ src/components/LeftMenu/NewNoteButton/NewNoteButton.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React from "react";
 import useStyles from "./styles";
-import {Button} from "@material-ui/core";
-import AddBoxIcon from '@material-ui/icons/AddBox';
-import createNewNote from '../../../firebaseFunctions/createNewNote';
+import { Button, Slide, Snackbar } from "@material-ui/core";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import createNewNote from "../../../firebaseFunctions/createNewNote";
 
 const NewNoteButton = () => {
-    const classes = useStyles();
-    return (
-        <Button
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        startIcon={<AddBoxIcon />}
-        fullWidth={true}
-        onClick={createNewNote}
-      >
-        New Note
-      </Button>
-    )
-}
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [transition, setTransition] = React.useState(undefined);
 
-export default NewNoteButton
+  function TransitionLeft(props) {
+    return <Slide {...props} direction="left" />;
+  }
+
+  const handleClick = (Transition) => {
+    setTransition(() => Transition);
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCreateNewNote = () => {
+    createNewNote();
+    handleClick(TransitionLeft);
+
+  }
+  return (
+    <>
+    <Button
+      variant="contained"
+      color="secondary"
+      className={classes.button}
+      startIcon={<AddBoxIcon />}
+      fullWidth={true}
+      onClick={handleCreateNewNote}
+    >
+      New Note
+    </Button>
+
+    <Snackbar
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={transition}
+        message="Note Created!"
+        key={transition ? transition.name : ""}
+        autoHideDuration={1000}
+      />
+      </>
+  );
+};
+
+export default NewNoteButton;
